dbe1910cf8f56f2292fb2588e8e58c04e067a5ab
5237-mests <msfnmulgeta@gmail.com>
2023-01-21 20:37:48 +0300

    Tuple[]

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Module for task 0
 """
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns start and end of page selected."""
     start = (page - 1) * page_size
     end = start + page_size
-    return tuple([start, end])
+    return (start, end)
