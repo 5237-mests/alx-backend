a116324f36c117e6063449ddd48eadbecc231ed6
5237-mests <msfnmulgeta@gmail.com>
2023-01-27 10:54:23 +0300

    put

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,6 +15,8 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """put data data into dict"""
+        if key is None or item is None:
+            return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 last_key, _ = self.cache_data.popitem(True)
