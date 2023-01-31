877b62100fdfe58be1bdaca19b8d5f068bbdc236
5237-mests <msfnmulgeta@gmail.com>
2023-01-31 19:50:18 +0300

    getlocale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
+"""module"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
-
-app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DFAULT_TIMEZONE'] = 'UTC' 
-
-babel = Babel(app)
 
 
 class Config(object):
     """config cls"""
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
 
 @babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(Config.LANGUAGES)
+def get_locale() -> str:
+    """get locale"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """route index"""
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
+    """main"""
     app.run(debug=True)
