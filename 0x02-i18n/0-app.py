db797c97422617258e5746bd8f3ae5337f468c50
5237-mests <msfnmulgeta@gmail.com>
2023-01-31 18:57:50 +0300

    ed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""modul"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,8 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """route func"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
+    """main"""
     app.run(debug=True)
