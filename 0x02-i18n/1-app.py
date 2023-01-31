a99075cd64854642319773b303e860a38e3dc11b
5237-mests <msfnmulgeta@gmail.com>
2023-01-31 19:30:25 +0300

    edd

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,7 +8,7 @@ class Config(object):
     """config cls"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
